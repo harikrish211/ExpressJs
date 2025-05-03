e37121857c4f710f84fe38274a4d9eab720efac7
harikrish211 <harikrishnapradeep2121@gmail.com>
2025-05-03 09:35:32 +0530

    routes example

---
+++ routesbasics.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+
+// GET /orders
+app.get('/orders', (req, res) => {
+  res.send('Here is the list of all orders.');
+});
+
+// POST /orders
+app.post('/orders', (req, res) => {
+  res.send('A new order has been created.');
+});
+
+// GET /users
+app.get('/users', (req, res) => {
+  res.send('Here is the list of all users.');
+});
+
+// POST /users
+app.post('/users', (req, res) => {
+  res.send('A new user has been added.');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
